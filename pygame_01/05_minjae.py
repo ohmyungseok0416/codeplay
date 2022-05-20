6dd51edd799eb6d25dd68edd00275e9c09d1235d
ohmyungseok0416 <codeplay.oms@gmail.com>
2022-05-20 18:36:15 +0900

    선생니 은 빡빠 ㄱ이다

--- pygame_01/05_minjae.py
+++ pygame_01/05_minjae.py
@@ -6,8 +6,8 @@ import random
 
 pygame.init()
 
-screen_width = 480
-screen_height = 640
+screen_width = 800
+screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("민재 피하기-코도폴레이")
@@ -19,7 +19,7 @@ clock = pygame.time.Clock()
 
 
 
-bg = pygame.image.load("pygame_01/source/bg.png")
+bg = pygame.image.load("pygame_01/source/bg2.png")
 
 character = pygame.image.load("pygame_01/source/character.png")
 
@@ -27,22 +27,71 @@ character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
 character_xPos = (screen_width / 2) - (character_width / 2)
-character_yPos = screen_height - character_height
+character_yPos = (screen_height / 2) - (character_height / 2)
 
+to_y = 0
 to_x = 0
-character_speed = 10
-
-enemy = pygame.image.load("pygame_01/source/enemy.png")
-
-enemy_size = enemy.get_rect().size
-enemy_width = enemy_size[0]
-enemy_height = enemy_size[1]
-enemy_xPos = random.randint(0, (screen_width - enemy_width))
-enemy_yPos = 0
+character_speed = 50
 
 
 
-enemy_speed = 10
+enemy1 = pygame.image.load("pygame_01/source/enemy.png")
+
+enemy1_size = enemy1.get_rect().size
+enemy1_width = enemy1_size[0]
+enemy1_height = enemy1_size[1]
+enemy1_xPos = random.randint(0, (screen_width - enemy1_width))
+enemy1_yPos = 0
+
+
+
+enemy1_speed = 10
+
+
+
+
+enemy2 = pygame.image.load("pygame_01/source/enemy.png")
+
+enemy2_size = enemy2.get_rect().size
+enemy2_width = enemy2_size[0]
+enemy2_height = enemy2_size[1]
+enemy2_xPos = screen_width - enemy2_width
+enemy2_yPos =random.randint(0, (screen_height - enemy2_height))
+
+
+
+enemy2_speed = 10
+
+
+
+
+
+enemy3 = pygame.image.load("pygame_01/source/enemy.png")
+
+enemy3_size = enemy3.get_rect().size
+enemy3_width = enemy3_size[0]
+enemy3_height = enemy3_size[1]
+enemy3_xPos = random.randint(0, (screen_width - enemy3_width))
+enemy3_yPos = screen_height - enemy3_height
+
+
+
+enemy3_speed = 10
+
+
+
+
+enemy4 = pygame.image.load("pygame_01/source/enemy.png")
+
+enemy4_size = enemy4.get_rect().size
+enemy4_width = enemy4_size[0]
+enemy4_height = enemy4_size[1]
+enemy4_xPos = 0
+enemy4_yPos = random.randint(0, (screen_height - enemy4_height))
+
+
+
+enemy4_speed = 10
 
 
 
@@ -59,15 +108,26 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                to_x -= 1
+                to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
-                to_x += 1
+                to_x += character_speed
           
+            elif event.key == pygame.K_UP:
+                to_y -= character_speed
+
+            elif event.key == pygame.K_DOWN:
+                to_y += character_speed
+
+
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
 
-    character_xPos += to_x * dt
+    character_xPos += to_x
+    character_yPos += to_y
 
     if character_xPos < 0:
         character_xPos = 0
@@ -79,20 +139,96 @@ while running:
     character_rect.left = character_xPos
     character_rect.top = character_yPos
 
-    enemy_rect = enemy.get_rect()
-    enemy_rect.left = enemy_xPos
-    enemy_rect.top = enemy_yPos
+    
+    enemy1_rect = enemy1.get_rect()
+    enemy1_rect.left = enemy1_xPos
+    enemy1_rect.top = enemy1_yPos
 
-    enemy_yPos += enemy_speed
-    if enemy_yPos > screen_height:
-        enemy_yPos = 0
-        enemy_xPos = random.randint(0, (screen_width - enemy_width))
-        enemy_speed = random.randint(15, 35)
+    enemy1_yPos -= enemy1_speed
+    if enemy1_yPos < 0:
+        enemy1_yPos = screen_height - enemy1_height
+        enemy1_xPos = random.randint(0, (screen_width - enemy1_width))
+        enemy1_speed = random.randint(5, 8)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    enemy3_rect = enemy3.get_rect()
+    enemy3_rect.left = enemy3_xPos
+    enemy3_rect.top = enemy3_yPos
+
+    enemy3_yPos -= enemy3_speed
+    if enemy3_yPos < 0:
+        enemy3_yPos = screen_height - enemy3_height
+        enemy3_xPos = random.randint(0, (screen_width - enemy3_width))
+        enemy3_speed = random.randint(5, 8)
+
+
+    enemy2_rect = enemy2.get_rect()
+    enemy2_rect.left = enemy2_xPos
+    enemy2_rect.top = enemy2_yPos
+
+    enemy2_xPos -= enemy2_speed
+    if enemy2_xPos < 0:
+        enemy2_yPos = random.randint(0, (screen_height - enemy2_height))
+        enemy2_xPos = screen_width - enemy2_width
+        enemy2_speed = random.randint(5, 8)
 
 
 
 
-    if character_rect.colliderect(enemy_rect):
+
+
+
+
+    enemy4_rect = enemy4.get_rect()
+    enemy4_rect.left = enemy4_xPos
+    enemy4_rect.top = enemy4_yPos
+
+    enemy4_xPos += enemy4_speed
+    if enemy4_xPos < 0:
+        enemy4_yPos = random.randint(0, (screen_height - enemy4_height))
+        enemy4_xPos = screen_width - enemy4_width
+        enemy4_speed = random.randint(4, 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if character_rect.colliderect(enemy1_rect) or    character_rect.colliderect(enemy2_rect) or   character_rect.colliderect(enemy3_rect)  or   character_rect.colliderect(enemy4_rect):
         print("fucking shit")
         running = False
 
@@ -104,8 +240,10 @@ while running:
 
     screen.blit(bg, (0, 0))
     screen.blit(character, (character_xPos, character_yPos))
-    screen.blit(enemy, (enemy_xPos, enemy_yPos))
-
+    screen.blit(enemy1, (enemy1_xPos, enemy1_yPos))
+    screen.blit(enemy2, (enemy2_xPos, enemy2_yPos))
+    screen.blit(enemy3, (enemy3_xPos, enemy3_yPos))
+    screen.blit(enemy4, (enemy4_xPos, enemy4_yPos))
     pygame.display.update()
 
 pygame.quit()
