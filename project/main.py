213e58a23175f3dfe82694210cba1dbc368253cd
jungshiwoo1011 <codeplay.shiwoo@gmail.com>
2022-09-23 19:03:25 +0900

    9월 23일 코딩 80퍼 완료

--- project/main.py
+++ project/main.py
@@ -2,18 +2,25 @@ import os
 from text_data_01 import questions_01
 from text_data_02 import questions_02
 from text_data_03 import questions_03
+from ending import *
+
+
 clear = lambda: os.system('cls')
 
 #3명의 인물 선택지
-women = ["선배", "동갑", "후배"]
+women = ["선배", "동갑", "후배", "끝"]
 
 #인물도전 진행상황 변수
+success = [0, 0, 0]
+
 score1 = 0 #인물별 연애점수
 score2 = 0 #인물별 연애점수
 score3 = 0 #인물별 연애점수
+success = [score1, score2, score3]
 
 #10개의 반복질문 함수
 def question10(woman):
+    score = 0
     clear()
     for question in woman:
         for i in question[0]:
@@ -25,9 +32,31 @@ def question10(woman):
         sel = int(input("1~4 중 하나를 선택 :"))
         print(f"주인공 : {question[1][sel-1]}")
         print(f"상대방 : {question[2][sel-1]}")
-        global score1 
-        score1 += question[3][sel-1]
+        score+= question[3][sel-1]
         clear()
+    if chosen_woman == "선배":
+        if score >= 12:
+            score1 = 1
+        else:
+            score1 = 0
+        print(personal_ending1[score1])
+
+    elif chosen_woman == "동갑":
+        if score >= 12:
+            score2 = 1
+        else:
+            score2 = 0
+        print(personal_ending2[score2])
+        
+    elif chosen_woman == "후배":
+        if score >= 12:
+            score3 = 1
+        else:
+            score3 = 0
+        print(personal_ending3[score3])
+    
+
+    input("(다음으로 넘어가려면 엔터를 치세요...")
 
 
 running =True
@@ -46,7 +75,8 @@ while running:
             question10(questions_02)
         elif chosen_woman == "후배":
             question10(questions_03)
-
+        elif chosen_woman == "끝":
+            running = False
     else:
         print("세 명의 여자와 모두 사귀게 된 홍식, 그는 쓰레기로 낙인찍혀 영원히 사회에서 격리된다...")
         running = False
