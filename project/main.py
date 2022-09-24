f1a7e652e0ad7de64af3d28e687e1008fe4d9be4
jungshiwoo1011 <codplay.shiwoo@gmail.com>
2022-09-24 19:48:33 +0900

    9/24 남극비바비바비바

--- project/main.py
+++ project/main.py
@@ -19,26 +19,35 @@ score3 = 0 #인물별 연애점수
 success = [score1, score2, score3]
 
 #10개의 반복질문 함수
+
+def line():
+    print("-" * 30)
+
 def question10(woman):
     score = 0
-    clear()
     for question in woman:
-        for i in question[0]:
+        clear()
+        for i in question [0]:
             print(i)
             num = 1
+        line()
         for answer in question[1]:
-            print(num, answer)
+            print(f"{num}.  {answer}")
             num += 1
+        line()
+        
         sel = int(input("1~4 중 하나를 선택 :"))
         print(f"주인공 : {question[1][sel-1]}")
         print(f"상대방 : {question[2][sel-1]}")
         score+= question[3][sel-1]
-        clear()
+        input("(다음으로 넘어가려면 엔터를 치세요...)")
+
     if chosen_woman == "선배":
         if score >= 12:
             score1 = 1
         else:
             score1 = 0
+        clear()
         print(personal_ending1[score1])
 
     elif chosen_woman == "동갑":
@@ -46,6 +55,7 @@ def question10(woman):
             score2 = 1
         else:
             score2 = 0
+        clear()
         print(personal_ending2[score2])
         
     elif chosen_woman == "후배":
@@ -53,9 +63,10 @@ def question10(woman):
             score3 = 1
         else:
             score3 = 0
+        clear()
         print(personal_ending3[score3])
     
-
+    
     input("(다음으로 넘어가려면 엔터를 치세요...")
 
 
@@ -65,8 +76,12 @@ while running:
     #누구와 연애를 할 지 선택
     if len(women) > 0:
         print("이성의 눈에 뜬 주인공, 여자친구를 만들고싶다")
+        line()
+        name_list = 1
         for name in women:
-             print(name)  
+            print(f"{name_list}. {name}")  
+            name_list += 1
+        line()
         choice = int(input("누구와 연애를 시작하시겠습니까? : "))
         chosen_woman = women.pop(choice-1)
         if chosen_woman == "선배":
@@ -84,8 +99,23 @@ while running:
 
 
 
-
-
+clear()
+if success[0] == 1 and success[1] == 1 and success[2] == 1:
+    print(real_ending111)
+elif success[0] == 0 and success[1] == 0 and success[2] == 0:
+    print(real_ending000)
+elif success[0] == 0 and success[1] == 0 and success[2] == 1:
+    print(real_ending001)
+elif success[0] == 0 and success[1] == 1 and success[2] == 1:
+    print(real_ending011)
+elif success[0] == 1 and success[1] == 1 and success[2] == 0:
+    print(real_ending110)
+elif success[0] == 1 and success[1] == 0 and success[2] == 1:
+    print(real_ending101)
+elif success[0] == 1 and success[1] == 0 and success[2] == 0:
+    print(real_ending100)
+elif success[0] == 0 and success[1] == 1 and success[2] == 0:
+    print(real_ending010)
 
 
 
