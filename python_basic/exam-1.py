7214b774332206a0dd6d4e356249761c367203ca
ohmyungseok0416 <the.0920.yule@gmail.com>
2022-12-09 18:57:24 +0900

    잉기모랑

---
+++ python_basic/exam-1.py
@@ -0,0 +1,12 @@
+import random
+p = 0
+who = ["명석", "헝섹", "지현", "서윤"]
+where = ["화장실", "교실", "운동장", "편의점"]
+what = ["양말", "지우개", "샤프심", "교통카드"]
+
+print(f"{random.choice(who)}(이)가 {random.choice(where)}에서 {random.choice(what)}로 살해했습니다")
+random.choice(where)
+random.choice(what)
+
+
+
